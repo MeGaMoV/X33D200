4cada1d029629d9383270d242e322e62f4851837
Nicolas Patouillard <contact@megamov.fr>
2022-05-14 04:50:44 +0200

    Ajout OSC

--- ESP_EMETTEUR/ESP_EMETTEUR.ino
+++ ESP_EMETTEUR/ESP_EMETTEUR.ino
@@ -1,19 +1,13 @@
-//MAC MAITRE 50:02:91:86:88:F0
-//PICOx : VERTE : D8:A0:1D:47:70:18
-//PICOy : JAUNE : 50:02:91:86:88:A0
-//PICO3 : ROUGE  : 50:02:91:86:88:F0
-
-#include <esp_now.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ESPmDNS.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
 
-// Actualisation de 50ms minimum
-#define MIN_REFRESH_TIME 50
+// Delay minimum entre deux messages OSC
+#define MIN_OSC_DELAY 15
 #define CASES 8
-
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t PICOxAddress[] = {0xD8, 0xA0, 0x1D, 0x47, 0x70, 0x18};
-uint8_t PICOyAddress[] = {0x50, 0x02, 0x91, 0x86, 0x88, 0xA0};
-uint8_t PICO3Address[] = {0x50, 0x02, 0x91, 0x86, 0x88, 0xF0};
 
 // Structure de fixture
 struct fixture {
@@ -36,171 +30,209 @@ typedef struct struct_message {
   byte fonction;
 } struct_message;
 
-// Create a struct_message called 
-struct_message espnow_msg;
+WiFiUDP Udp;
+const char* ssid = "Private";
+const char* password = "zaza4014";
 
-esp_now_peer_info_t peerInfo;
+const IPAddress PicoX(192, 168, 0, 103);
+const IPAddress PicoY(192, 168, 0, 108);
+const IPAddress Pico3(192, 168, 0, 254);
 
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-}
+const unsigned int port = 8888; //Port de reception des informations sur les Picos
+const unsigned int localPort = 8888; // Port local pour écouter en OSC sur cet emetteur
+
+// Tableau de carré
+carre matrice[8][8] = {};
+
+//-----------------------------SETUP-------------------------------------
 
 void setup() {
-  // Init Serial Monitor
-  Serial.begin(9600);
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) return;
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-
-  // Register peer
-  memcpy(peerInfo.peer_addr, PICOxAddress, 6);
-  //peerInfo.channel = 0;
-  //peerInfo.encrypt = false;
-  // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add PicoX");
-    return;
-  }
-  memcpy(peerInfo.peer_addr, PICOyAddress, 6);
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add PicoY");
-    return;
-  }
-  memcpy(peerInfo.peer_addr, PICO3Address, 6);
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add Pico3");
-    return;
-  }
+  Serial.begin(115200);
+  
+  WiFi.disconnect(true);
+  WiFi.onEvent(Wifi_connected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(Get_IPAddress, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(Wifi_disconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.begin(ssid, password);
+  Udp.begin(localPort);
 }
 
 void loop() {
-  blueMatrice();
-  delay(1000);
+  rgbMatrice();
+  envoyerIntensite(25);
+  envoyerMatrice();
+  envoyerPrint();
+
+  delay(2000);
+  
   blackMatrice();
+  envoyerClear();
   delay(1000);
+}
+
+/////////////////// FONCTIONS D'ENVOI OSC /////////////////
+void envoyerFixture(short x, short y, short fixture){
+  char laTrame[15];
+  uint8_t donnee[3];
+  
+  sprintf(laTrame, "/fixture/%d/%d/%d", x, y, fixture);
+  OSCMessage msg(laTrame);
+
+  donnee[0] = matrice[x - 1][y - 1].fixtures[fixture].r;
+  donnee[1] = matrice[x - 1][y - 1].fixtures[fixture].g;
+  donnee[2] = matrice[x - 1][y - 1].fixtures[fixture].b;
+  
+  msg.add(donnee, sizeof(donnee));
+  Udp.beginPacket(PicoX, port);
+  msg.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(PicoY, port);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
+
+void envoyerCarre(short x, short y){
+  char laTrame[11];
+  uint8_t donnee[12];
+  short posTotale = 0;
+  
+  sprintf(laTrame, "/carre/%d/%d", x, y);
+  OSCMessage msg(laTrame);
+
+  for(short tmpFix = 0; tmpFix < 4; tmpFix++){
+    donnee[posTotale] = matrice[x - 1][y - 1].fixtures[tmpFix].r;
+    donnee[posTotale + 1] = matrice[x - 1][y - 1].fixtures[tmpFix].g;
+    donnee[posTotale + 2] = matrice[x - 1][y - 1].fixtures[tmpFix].b;
+    posTotale += 3;
+  }
+  
+  msg.add(donnee, sizeof(donnee));
+  Udp.beginPacket(PicoX, port);
+  msg.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(PicoY, port);
+  msg.send(Udp);
+  Udp.endPacket();
+  
+  msg.empty();
+}
+
+void envoyerMatrice(){
+  for(short y = 1; y <= 8; y++){
+    for(short x = 1; x <= 8; x++){
+      envoyerCarre(x,y);
+      delay(MIN_OSC_DELAY);
+    }
+  }
+}
+
+void envoyerClear(){
+  OSCMessage msg("/clear");
+  Udp.beginPacket(PicoX, port);
+  msg.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(PicoY, port);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
+
+void envoyerPrint(){
+  OSCMessage msg("/print");
+  Udp.beginPacket(PicoX, port);
+  msg.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(PicoY, port);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
+
+void envoyerIntensite(int intensite){
+  OSCMessage msg("/intensite");
+  msg.add(intensite);
+  Udp.beginPacket(PicoX, port);
+  msg.send(Udp);
+  Udp.endPacket();
+
+  Udp.beginPacket(PicoY, port);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
+
+////////// FONCTIONS ALLUMAGE MATRICE //////////
+void redMatrice(){
+  for(short y = 0; y < 8; y++){
+    for(short x = 0; x < 8; x++){
+      for(short f = 0; f <4; f++){
+        matrice[x][y].fixtures[f].r = 255;
+        matrice[x][y].fixtures[f].g = 0;
+        matrice[x][y].fixtures[f].b = 0;
+      }
+    }
+  }
 }
 
 void blueMatrice(){
   for(short y = 0; y < 8; y++){
     for(short x = 0; x < 8; x++){
       for(short f = 0; f <4; f++){
-        espnow_msg.matrice[x][y].fixtures[f].r = 0;
-        espnow_msg.matrice[x][y].fixtures[f].g = 0;
-        espnow_msg.matrice[x][y].fixtures[f].b = 255;
+        matrice[x][y].fixtures[f].r = 0;
+        matrice[x][y].fixtures[f].g = 0;
+        matrice[x][y].fixtures[f].b = 255;
       }
     }
   }
-  esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
+}
+
+void greenMatrice(){
+  for(short y = 0; y < 8; y++){
+    for(short x = 0; x < 8; x++){
+      for(short f = 0; f <4; f++){
+        matrice[x][y].fixtures[f].r = 0;
+        matrice[x][y].fixtures[f].g = 255;
+        matrice[x][y].fixtures[f].b = 0;
+      }
+    }
+  }
+}
+
+void whiteMatrice(){
+  for(short y = 0; y < 8; y++){
+    for(short x = 0; x < 8; x++){
+      for(short f = 0; f <4; f++){
+        matrice[x][y].fixtures[f].r = 255;
+        matrice[x][y].fixtures[f].g = 255;
+        matrice[x][y].fixtures[f].b = 255;
+      }
+    }
+  }
 }
 
 void blackMatrice(){
   for(short y = 0; y < 8; y++){
     for(short x = 0; x < 8; x++){
       for(short f = 0; f <4; f++){
-        espnow_msg.matrice[x][y].fixtures[f].r = 0;
-        espnow_msg.matrice[x][y].fixtures[f].g = 0;
-        espnow_msg.matrice[x][y].fixtures[f].b = 0;
+        matrice[x][y].fixtures[f].r = 0;
+        matrice[x][y].fixtures[f].g = 0;
+        matrice[x][y].fixtures[f].b = 0;
       }
     }
   }
-  esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
 }
 
-/*
-void caseMusicale(int nbJoueurs){
-  for(int i = nbJoueurs; i > 0; i--){
-    blackAllGrid();
-    delay(3000);
-    randomOfGrid(i);
-    delay(5000);
-  }
-}
-
-void whiteAllGrid() {
-  for (int i = 1; i <= CASES; i++) {
-    for (int j = 1; j <= CASES; j++) {
-      espnow_msg.col = j;
-      espnow_msg.ligne = i;
-      espnow_msg.fixture = 1;
-      espnow_msg.R = 255;
-      espnow_msg.G = 255;
-      espnow_msg.B = 255;
-      espnow_msg.intensite = 255;
-      espnow_msg.fonction = 0;
-      // Send message via ESP-NOW
-      esp_err_t result = esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
-      delay(MIN_REFRESH_TIME);
-    }
-  }
-}
-
-void randomOfGrid(short nCasesAleatoires) {
-  short couleurActuelle = 0;
-  short r, g, b = 0;
-  bool casesFaites[9][9]; //9 même si on utilise que de 1 à 8
-  short x = random(8) + 1;
-  short y = random(8) + 1;
-
-  //Reinitialiser le tableau des cases faites
-  for(int resetY = 0; resetY < 9; resetY++){
-    for(int resetX = 0; resetX < 9; resetX++){
-      casesFaites[resetX][resetY] = false;
-    }
-  }
-
-  // Set N random cases to send
-  for (int i = 1; i <= nCasesAleatoires; i++) {
-    do{
-      x = random(8) + 1;
-      y = random(8) + 1;
-    } while(casesFaites[x][y] == true);
-
-    switch (couleurActuelle) {
-      case 0:
-        r = random(255);
-        g = random(25);
-        b = random(50);
-        couleurActuelle++;
-        break;
-      case 1:
-        r = random(25);
-        g = random(255);
-        b = random(50);
-        couleurActuelle++;
-        break;
-      case 2:
-        r = random(25);
-        g = random(50);
-        b = random(255);
-        couleurActuelle = 0;
-        break;
-    }
-
-    espnow_msg.col = x;
-    espnow_msg.ligne = y;
-    espnow_msg.fixture = 1;
-    espnow_msg.R = 0;
-    espnow_msg.G = 0;
-    espnow_msg.B = 255;
-    espnow_msg.intensite = 255;
-    espnow_msg.fonction = 0;
-
-    // Send message via ESP-NOW
-    casesFaites[x][y] = true;
-    esp_err_t result = esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
-  }
-}
-
-void rgbAllGrid() {
+void rgbMatrice(){
   short r, g, b = 0;
   short couleurActuelle = 0;
 
-  for (int i = 1; i <= CASES; i++) {
-    for (int j = 1; j <= CASES; j++) {
+  for (int y = 1; y <= CASES; y++) {
+    for (int x = 1; x <= CASES; x++) {
       switch (couleurActuelle) {
         case 0:
           r = 255;
@@ -222,35 +254,33 @@ void rgbAllGrid() {
           break;
       }
 
-      espnow_msg.col = j;
-      espnow_msg.ligne = i;
-      espnow_msg.fixture = 1;
-      espnow_msg.R = r;
-      espnow_msg.G = g;
-      espnow_msg.B = b;
-      espnow_msg.intensite = 255;
-      espnow_msg.fonction = 0;
-      // Send message via ESP-NOW
-      esp_err_t result = esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
-      delay(MIN_REFRESH_TIME);
+      for(short tmpFix = 0; tmpFix <= 3; tmpFix++){
+        matrice[x - 1][y - 1].fixtures[tmpFix].r = r;
+        matrice[x - 1][y - 1].fixtures[tmpFix].g = g;
+        matrice[x - 1][y - 1].fixtures[tmpFix].b = b;
+      }
     }
   }
 }
+////////////////////////////////////////////////////////
 
-void blackAllGrid() {
-  for (int i = 1; i <= CASES; i++) {
-    for (int j = 1; j <= CASES; j++) {
-      espnow_msg.col = j;
-      espnow_msg.ligne = i;
-      espnow_msg.fixture = 1;
-      espnow_msg.R = 0;
-      espnow_msg.G = 0;
-      espnow_msg.B = 0;
-      espnow_msg.intensite = 0;
-      espnow_msg.fonction = 0;
-      // Send message via ESP-NOW
-      esp_err_t result = esp_now_send(0, (uint8_t *) &espnow_msg, sizeof(espnow_msg));
-      delay(MIN_REFRESH_TIME);
-    }
-  }
-}*/
+
+/////////////////// FONCTIONS WIFI ///////////////////
+void Wifi_connected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Successfully connected to Access Point");
+}
+
+void Get_IPAddress(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("WIFI is connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void Wifi_disconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Disconnected from WIFI access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.disconnected.reason);
+  Serial.println("Reconnecting...");
+  WiFi.begin(ssid, password);
+  Udp.begin(localPort);
+}
--- PICOy/PICOy.ino
+++ PICOy/PICOy.ino
@@ -1,5 +1,10 @@
-#include <esp_now.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
 #include <FastLED.h>
 
 #define DATA_PIN 25
@@ -13,8 +18,18 @@
 #define LARGEUR_FIXTURE 9
 #define COTES 8
 
-////////////////////////////////////
+/////// CODE TINYPICO X ou Y ///////
+bool PicoX = false;
 
+////////////////////////////////////
+// Gestion OSC
+WiFiUDP Udp;
+const char* ssid = "Private";
+const char* password = "zaza4014";
+const unsigned int localPort = 8888;
+uint8_t bFixture[3]; //Stockage données OSC fixture
+uint8_t bCarre[12];  //Stockage données OSC carree
+  
 // Structure de fixture
 struct fixture {
   short rang;
@@ -23,79 +38,138 @@ struct fixture {
   short b;
 };
 
-// Un carré, avec ses fixtures horizontales 1, 2, 5 et 6
+// Un carré, avec ses 4 fixtures dans un tableau
 struct carre {
   fixture fixtures[4];
   short pos = 0;
 };
 
-// Structure d'un message reçu via ESP-NOW
-typedef struct espnow_msg {
-  carre matrice[8][8];
-  byte intensite;
-  byte fonction;
-} espnow_msg;
-
-// Message reçu type "espnow_msg"
-espnow_msg messageRecu;
-
 // Tableau de carré
 carre matrice[8][8] = {};
+
+// String fixture carre OSC pour parsing
+String carreX, carreY, numFixture;
 
 // Position des LED inversées
 // Exemple : 0 pour des LED 0 à 149
 short ledInverses[] = {0, 2249};
 
-// Lignes inversées - différentes de ORDRE_LED (RGB / GRB)
-short lignesInverses[] = {1, 16};
-
 CRGB rawleds[LIGNES * LED_PAR_LIGNE];
 CRGBSet leds(rawleds, LIGNES * LED_PAR_LIGNE);
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(115200);
+
+  // Initialisation Wifi
+  WiFi.disconnect(true);
+  WiFi.onEvent(Wifi_connected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(Get_IPAddress, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(Wifi_disconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.begin(ssid, password);
+  Udp.begin(localPort);
 
   //Initialisation de toutes les LED, nettoyage et luminosité
   FastLED.addLeds<CHIPSET, DATA_PIN, ORDRE_LED>(leds, LIGNES * LED_PAR_LIGNE);
-  FastLED.clear();
   FastLED.setBrightness(BRIGHTNESS);
-  initialisationPositionmatrice();
 
-  // Ecouter en ESP-NOW
-  // Mode "Wifi station" et initialisation
-  WiFi.mode(WIFI_STA);
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // On se prépare à recevoir une trame
-  esp_now_register_recv_cb(majMatrice);
-}
+  //Initialiser la position des LED sur la matrice
+  initialisationPositionMatrice();
 
-void loop() {
-}
-
-void majMatrice(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&messageRecu, incomingData, sizeof(messageRecu));
-
-  for(short y = 0; y < 8; y++){
-    for(short x = 0; x < 8; x++){
-      for(short f = 0; f <4; f++){
-        matrice[x][y].fixtures[f].r = messageRecu.matrice[x][y].fixtures[f].r;
-        matrice[x][y].fixtures[f].g = messageRecu.matrice[x][y].fixtures[f].g;
-        matrice[x][y].fixtures[f].b = messageRecu.matrice[x][y].fixtures[f].b;
-      }
-    }
-  }
-
-  afficherMatrice();
- 
+  //Tout à zero
+  couleurMatrice(0,0,0);
+  majMatrice();
   FastLED.show();
 }
 
+void loop() {
+  OSCMessage msg;
+  int size;
+  char strAddress[100];
+  String addressSTO;
+
+  // Tant que l'on reçoit quelque-chose en UDP
+  if((size = Udp.parsePacket()) > 0)
+  {
+    //Recuperation du flux UDP dans msg
+    while(size--) msg.fill(Udp.read());
+
+    //On récupère l'adresse
+    msg.getAddress(strAddress, 0);
+    addressSTO = (char*)strAddress;
+
+    if (addressSTO == "/print") {
+      majMatrice();
+      FastLED.show();
+    }
+
+    if (addressSTO == "/clear") {
+      couleurMatrice(0,0,0);
+      majMatrice();
+      FastLED.show();
+    }
+
+    if (msg.match("/intensite") > 0) {
+      int intensite = msg.getInt(0);
+      if(intensite <= 255) FastLED.setBrightness(intensite);
+    }
+    
+    if (msg.match("/fixture") > 0) {
+      carreX = addressSTO.substring(9, 10);
+      carreY = addressSTO.substring(11, 12);
+      numFixture = addressSTO.substring(13);
+      msg.getBlob(0, bFixture, 3);
+      majFixture(carreX.toInt(), carreY.toInt(), numFixture.toInt());
+    }
+    
+    if (msg.match("/carre") > 0) {
+      carreX = addressSTO.substring(7, 8);
+      carreY = addressSTO.substring(9);
+      msg.getBlob(0, bCarre, 24);
+      //Serial.println((String)carreX+" "+carreY);
+      majCarre(carreX.toInt(), carreY.toInt());
+    }
+  }
+  // END LOOP
+}
+
+////////////////// PROGRAMMES DE MISE A JOUR //////////////////////
+void majFixture(int carreX, int carreY, int numFixture) {
+  carreX--;
+  carreY--;
+  if(carreX < 0 || carreX > COTES - 1 || carreY < 0 || carreY > COTES - 1) return;
+  matrice[carreX][carreY].fixtures[numFixture].r = bFixture[0];
+  matrice[carreX][carreY].fixtures[numFixture].g = bFixture[1];
+  matrice[carreX][carreY].fixtures[numFixture].b = bFixture[2];
+}
+
+void majCarre(int carreX, int carreY) {
+  int posTotal = 0;
+  carreX--;
+  carreY--;
+  if(carreX < 0 || carreX > COTES - 1 || carreY < 0 || carreY > COTES - 1) return;
+  for (int i = 0; i < 4; i++) {
+    matrice[carreX][carreY].fixtures[i].r = bCarre[posTotal];
+    matrice[carreX][carreY].fixtures[i].g = bCarre[posTotal + 1];
+    matrice[carreX][carreY].fixtures[i].b = bCarre[posTotal + 2];
+    posTotal += 3;
+  }
+}
+
+////////////// FONCTIONS GENERALES DE LA MATRICE /////////////////
+void couleurMatrice(short r, short g, short b) {
+  for (short y = 0; y < 8; y++) {
+    for (short x = 0; x < 8; x++) {
+      for (short f = 0; f < 4; f++) {
+        matrice[x][y].fixtures[f].r = r;
+        matrice[x][y].fixtures[f].g = g;
+        matrice[x][y].fixtures[f].b = b;
+      }
+    }
+  }
+}
+
 // Fonction qui initialise tous les matrice et leurs fixtures respectives
-void initialisationPositionmatrice() {
+void initialisationPositionMatrice() {
   short posx = 0; //Pos X du carré en cours
   short posy = 0; //Pos Y du carré en cours
   short posFixture = 0; //Incrémente à chaque fixture
@@ -115,65 +189,110 @@ void initialisationPositionmatrice() {
 
       // Nouveau carré (une fixture sur 2)
       if (posFixture % 2 == 0) {
+        
         // On avance ou "recul" vision "carrés"
-        if (ordreActuel) posx++; else posx--;
+        if(PicoX){
+          if (ordreActuel) posx++; else posx--;
+        } else {
+          if (ordreActuel) posy++; else posy--;
+        }
 
         // On est de retour au début de ligne ou à la fin
-        if (posx < 0 || posx * 2 > 15) {
-          decalageEnCours += DECALAGE_PAR_COTE * 2; //On ajoute le decalage
-          if (ordreActuel) {
-            posx = 7;
-            ordreActuel = false;
-          } else {
-            posx = 0;
-            ordreActuel = true;
-            posy++;
+        if(PicoX){ //version PicoX
+          if (posx < 0 || posx * 2 > LIGNES - 1) {
+            decalageEnCours += DECALAGE_PAR_COTE * 2; //On ajoute le decalage
+            if (ordreActuel) {
+              posx = COTES - 1;
+              ordreActuel = false;
+            } else {
+              posx = 0;
+              ordreActuel = true;
+              posy++;
+            }
           }
+          // Si on sort du tableau
+          if (posy * 2 >= LIGNES) break;
+          
+        } else { //version PicoY
+          
+          // On est de retour au début de ligne ou à la fin
+          if (posy < 0 || posy * 2 > LIGNES - 1) {
+            decalageEnCours += DECALAGE_PAR_COTE * 2; //On ajoute le decalage
+            if (ordreActuel) {
+              posy = COTES - 1;
+              ordreActuel = false;
+            } else {
+              posy = 0;
+              ordreActuel = true;
+              posx++;
+            }
+          }
+          // Si on sort du tableau
+          if (posx * 2 >= LIGNES) break;
         }
-        // Si on sort du tableau
-        if (posy * 2 >= 16) break;
       }
     }
   }
 }
 
 void ajouterNouvelleFixture(short posx, short posy, short tmpPosLED) {
-  fixture maFixture = {0,0,0,0};
+  fixture maFixture = {0, 0, 0, 0};
   maFixture.rang = tmpPosLED;
   matrice[posx][posy].fixtures[matrice[posx][posy].pos] = maFixture;
   matrice[posx][posy].pos++;
 }
 
-void afficherMatrice(){
+void majMatrice() {
   short posDebut = 0;
   short posFin = 0;
 
   CRGB couleur = CRGB::Black;
 
-  for(short y = 0; y < 8; y++){
-    for(short x = 0; x < 8; x++){
-      for(short f = 0; f <4; f++){
+  for (short y = 0; y < 8; y++) {
+    for (short x = 0; x < 8; x++) {
+      for (short f = 0; f < 4; f++) {
 
         //Determiner la couleur
         couleur.r = matrice[x][y].fixtures[f].r;
         couleur.g = matrice[x][y].fixtures[f].g;
         couleur.b = matrice[x][y].fixtures[f].b;
 
+        posDebut = matrice[x][y].fixtures[f].rang;
+        posFin = matrice[x][y].fixtures[f].rang + LARGEUR_FIXTURE;
+
         // Cas des inversion de couleurs
-        for(int pos=0; pos < sizeof ledInverses/sizeof ledInverses[0]; pos++) {
-          if(posDebut >= ledInverses[pos] && posDebut <= ledInverses[pos] + LED_PAR_LIGNE){
+        for (int pos = 0; pos < sizeof ledInverses / sizeof ledInverses[0]; pos++) {
+          if (posDebut >= ledInverses[pos] && posDebut <= ledInverses[pos] + LED_PAR_LIGNE) {
             int tmpR = couleur.r;
             couleur.r = couleur.g;
             couleur.g = tmpR;
           }
         }
-  
-        posDebut = matrice[x][y].fixtures[f].rang;
-        posFin = matrice[x][y].fixtures[f].rang + LARGEUR_FIXTURE;
-
+        
         CRGBSet fixture(leds(posDebut, posFin));
         fill_solid(fixture, LARGEUR_FIXTURE, couleur);
       }
     }
   }
 }
+
+///////////////////  Fonctions WIFI ////////////////////////
+void Wifi_connected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Successfully connected to Access Point");
+}
+
+void Get_IPAddress(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("WIFI is connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void Wifi_disconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Disconnected from WIFI access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.disconnected.reason);
+  Serial.println("Reconnecting...");
+  WiFi.begin(ssid, password);
+  Udp.begin(localPort);
+}
+////////////////////////////////////

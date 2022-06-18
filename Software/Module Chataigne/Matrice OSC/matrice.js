5f5e3e338742c044f196bd25500d0c66df4dfe3f
MeGaMoV <contact@megamov.fr>
2022-06-18 17:24:46 +0200

    Added SPIRALE and INTERSECTION functions to module

--- Software/Module Chataigne/Matrice OSC/matrice.js
+++ Software/Module Chataigne/Matrice OSC/matrice.js
@@ -32,43 +32,167 @@ function sendSquare(x,y,color) {
   So you can send hundred of requests, and THEN apply with this print
   ref : https://www.reddit.com/r/FastLED/comments/aqlb94/troubleshooting_slow_performance_tied_to/
 */
-function print(){
+function sendPrint(){
   local.send("/print");
 }
 
 /*
   Clear function, this will instant turn off the LED's
 */
-function clear(){
+function sendClear(){
   local.send("/clear");
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+  Clear function, this will instant turn off the LED's
+*/
+function sendIntensity(intensity){
+  local.send("/intensity", intensity*2.55);
+}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
   Send an unique Fixture and turn it on
 */
-function lightFixture(x,y,f,color){
+function fixture(x,y,f,color){
   sendFixture(x,y,f,color);
-  print();
+  sendPrint();
 }
 
 /*
   Send an unique Square and turn it on
 */
-function lightSquare(x,y,color){
+function square(x,y,color){
   sendSquare(x,y,color);
-  print();
+  sendprint();
 }
 
 /*
   Function used to send an entire Matrix update based on each squares 
 */
-function lightMatrix(color) {
+function matrix(color) {
   for(y = 1; y <= 8; y++){
     for(x = 1; x <= 8; x++){
       sendSquare(x,y,color);
     }
   }
-  print();
+  sendPrint();
+}
+
+/*
+  Function used to create a "spirale"
+*/
+function spirale(taille, color){
+  if(taille == 4){
+    xyMin = 1;
+    xyMax = 8;
+  }
+  if(taille == 3){
+    xyMin = 2;
+    xyMax = 7;
+  }
+  if(taille == 2){
+    xyMin = 3;
+    xyMax = 6;
+  }
+  if(taille == 1){
+    xyMin = 4;
+    xyMax = 5;
+  }
+
+  for(pos = xyMin; pos <= xyMax; pos++){
+    //Left
+    sendFixture(xyMin, pos, 7,color);
+    sendFixture(xyMin, pos, 8,color);
+
+    //Right
+    sendFixture(pos, xyMin, 1,color);
+    sendFixture(pos, xyMin, 2,color);
+
+    //Top
+    sendFixture(xyMax, pos, 3,color);
+    sendFixture(xyMax, pos, 4,color);
+
+    //Bottom
+    sendFixture(pos, xyMax, 5,color);
+    sendFixture(pos, xyMax, 6,color);
+  }
+
+  sendPrint();
+}
+
+/*
+  Send color to an intersection between 2 squares
+*/
+function intersection(x1, y1, x2, y2, color){
+  if(x2 == x1){
+    posX = "MID";
+  } else {
+    if(x2 < x1){
+      posX = "LEFT";
+    } else {
+      posX = "RIGHT";
+    }
+  }
+
+  if(y2 == y1){
+    posY = "MID";
+  } else {
+    if(y2 < y1){
+      posY = "UP";
+    } else {
+      posY = "DOWN";
+    }
+  }
+  
+  if(posX+posY == "LEFTMID"){
+    sendFixture(x1, y1, 7,color);
+    sendFixture(x1, y1, 8,color);
+    sendFixture(x2, y2, 3,color);
+    sendFixture(x2, y2, 4,color);
+  }
+  if(posX+posY == "RIGHTMID"){
+    sendFixture(x1, y1, 3,color);
+    sendFixture(x1, y1, 4,color);
+    sendFixture(x2, y2, 7,color);
+    sendFixture(x2, y2, 8,color);
+  }
+  if(posX+posY == "MIDUP"){
+    sendFixture(x1, y1, 1,color);
+    sendFixture(x1, y1, 2,color);
+    sendFixture(x2, y2, 5,color);
+    sendFixture(x2, y2, 6,color);
+  }
+  if(posX+posY == "MIDDOWN"){
+    sendFixture(x1, y1, 5,color);
+    sendFixture(x1, y1, 6,color);
+    sendFixture(x2, y2, 1,color);
+    sendFixture(x2, y2, 2,color);
+  }
+  if(posX+posY == "LEFTUP"){
+    sendFixture(x1, y1, 1,color);
+    sendFixture(x1, y1, 8,color);
+    sendFixture(x2, y2, 4,color);
+    sendFixture(x2, y2, 5,color);
+  }
+  if(posX+posY == "RIGHTUP"){
+    sendFixture(x1, y1, 2,color);
+    sendFixture(x1, y1, 3,color);
+    sendFixture(x2, y2, 6,color);
+    sendFixture(x2, y2, 7,color);
+  }
+  if(posX+posY == "LEFTDOWN"){
+    sendFixture(x1, y1, 6,color);
+    sendFixture(x1, y1, 7,color);
+    sendFixture(x2, y2, 2,color);
+    sendFixture(x2, y2, 3,color);
+  }
+  if(posX+posY == "RIGHTDOWN"){
+    sendFixture(x1, y1, 4,color);
+    sendFixture(x1, y1, 5,color);
+    sendFixture(x2, y2, 1,color);
+    sendFixture(x2, y2, 8,color);
+  }
+
+  sendPrint();
 }
